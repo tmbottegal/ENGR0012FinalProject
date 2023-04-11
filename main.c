5638dbc306078880ce8b0d17b9cc892e2081478c
tmbottegal <38114233+tmbottegal@users.noreply.github.com>
2023-04-11 19:06:04 -0400

    added function guidelines

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
-#import <math.h>
-#import <stdio.h>
-#import <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void triviapool () {
+    // will have input for type of trivia
+}
+
+int questionpool1 () {
+    return 0;
+}
+
+int questionpool2() {
+    return 0;
+}
+
+int questionpool3() {
+    return 0;
+}
+
+void randomizeseed() {
+
+}
 
 int main () {
     return 0;
